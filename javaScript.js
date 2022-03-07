428e2e99954331d9e932742710ab759ee8dae2f9
Faiisu <sukumdecha@gmail.com>
2022-03-08 00:56:13 +0700

    Update00

---
+++ javaScript.js
@@ -0,0 +1,8 @@
+let inspiration_box = document.querySelector('#inspiration');
+let insiration_scroll = document.getElementById('inspiration::-webkit-scrollbar');
+var inspiration_box_width = inspiration_box.offsetWidth;
+
+
+function bottonClick(){
+    window.alert("ควยไรไอสัส");
+}
