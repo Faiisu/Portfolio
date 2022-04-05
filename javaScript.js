dfa88d2687f5f90c409979356733bb9ca8331a0d
Faiisu <sukumdecha@gmail.com>
2022-04-06 01:10:09 +0700

    update01

--- javaScript.js
+++ javaScript.js
@@ -2,7 +2,18 @@ let inspiration_box = document.querySelector('#inspiration');
 let insiration_scroll = document.getElementById('inspiration::-webkit-scrollbar');
 var inspiration_box_width = inspiration_box.offsetWidth;
 
-
 function bottonClick(){
     window.alert("ควยไรไอสัส");
 }
+
+// function Tripaloski_move(){
+//     var Tripaloski_move_left = 1000;
+//     document.getElementById("fontText").style.left = 900-Tripaloski_move_left;
+//     setTimeout(function () {
+//         if(Tripaloski_move_left >= 0){
+//             Tripaloski_move_left++;
+//             Tripaloski_move();
+//         }
+//     }, 50);
+// }
+// Tripaloski_move();
