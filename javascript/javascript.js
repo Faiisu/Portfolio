e5b4dda82c21ff7e6ca25ea6a442c9ea8b090a2d
faiisu <sukumdecha@gmail.com>
2022-10-22 00:30:31 +0700

    afsd

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -1,7 +1,7 @@
-let scrollInfo = document.querySelector("#scrolltrack");
 let scrollTrack = document.getElementById("pageTracker");
-let SelectContant = document.getElementById("SelectContant");
-let name_header = document.getElementById("name_header");
+
+//on open webside
+let HelloWorld = document.getElementById("HelloWorld");
 
 function getWidth() {
     return Math.max(
@@ -15,20 +15,37 @@ function getHeight() {
     );
 }
   
-console.log('Width:  ' +  getWidth() );
-console.log('Height: ' + getHeight() );
+// console.log('Width:  ' +  getWidth() );
+// console.log('Height: ' + getHeight() );
+const rect = document.getElementById("MyProfile").getBoundingClientRect();
 
 window.addEventListener("scroll", function(){
-    // console.log(window.scrollY/(document.body.scrollHeight-770) * getWidth());
-    scrollTrack.style.width = window.scrollY/(document.body.scrollHeight-770) * getWidth() + "px";    
+    scrollTrack.style.width = window.scrollY/(document.body.scrollHeight-770) * getWidth() + "px";
+
+    if(this.window.scrollX > 716.7999877929688);
+
 })
 
 function movePageto(moveto){
-    var moveto;
-    if(moveto=='move1') moveto = 716.7999877929688;
-    else if(moveto=='move2') moveto = 1516.800048828125;
-    else if(moveto=='move3') moveto = 2325.60009765625;
-    window.scroll({
-        top: moveto,
-    })
+    var moveat;
+    if(moveto == "move1") moveat = document.querySelector("#MyProfile").getBoundingClientRect().top;
+    if(moveto == "move2") moveat = document.querySelector("#Activity").getBoundingClientRect().top;
+    if(moveto == "move3") moveat = document.querySelector("#Certificate").getBoundingClientRect().top;
+
+    window.scrollTo(0,moveat+window.scrollY);
 }
+
+function changeWord(thatId, changeTo){
+    let _Element = document.getElementById(thatId);
+    _Element.innerHTML = changeTo;
+}
+
+var SecondSayHi = document.getElementById("SecondSayHi");
+var ThirdSayHi = document.getElementById("ThirdSayHi");
+SecondSayHi.addEventListener("animationend",()=>{
+    SecondSayHi.remove();
+})
+ThirdSayHi.addEventListener("animationend",()=>{
+    ThirdSayHi.remove();
+    window.scrollTo(0,document.querySelector("#MyProfile").getBoundingClientRect().top+window.scrollY);
+})
